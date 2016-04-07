461dcb56e25ab498d34faae7a74f1eb74ce875e6
15096134 <15096134@p162802.acd.puc-campinas.edu.br>
2016-04-07 20:14:56 -0300

    Classes base

--- Calculator/src/calculator/Queue.java
+++ Calculator/src/calculator/Queue.java
@@ -9,6 +9,47 @@ package calculator;
  *
  * @author 15096134
  */
-public class Queue {
+public class Queue<X> {
+        
+    public Queue(int maximumSize) throws Exception {
+        
+    }
     
+    public Queue(Queue s) throws Exception {
+        
+    }
+    
+    public void enqueue(X elm) throws Exception {
+        
+    }
+    
+    public X dequeue() throws Exception {
+        X dequeued = null;
+        
+        return dequeued;
+    }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Queue clonned = (Queue) super.clone();
+        
+        return clonned;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        
+        return hash;
+    }
+    
+    @Override
+    public String toString() {
+        return "Stack";
+    }
 }
--- Calculator/src/calculator/Stack.java
+++ Calculator/src/calculator/Stack.java
@@ -9,6 +9,47 @@ package calculator;
  *
  * @author 15096134
  */
-public class Stack {
+public class Stack<X> implements Cloneable {
     
+    public Stack(int maximumSize) throws Exception {
+        
+    }
+    
+    public Stack(Stack s) throws Exception {
+        
+    }
+    
+    public void push(X elm) throws Exception {
+        
+    }
+    
+    public X pop() throws Exception {
+        X poped = null;
+        
+        return poped;
+    }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        Queue clonned = (Queue) super.clone();
+        
+        return clonned;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        
+        return hash;
+    }
+    
+    @Override
+    public String toString() {
+        return "Queue";
+    }
 }
