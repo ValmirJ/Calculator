e286e3dae6e014520e84e93743e203f132f21a45
Calebe Lustosa Carvalho <yoshke23@gmail.com>
2016-04-09 15:21:30 -0300

    Added files via upload

--- Calculator/src/calculator/Queue.java
+++ Calculator/src/calculator/Queue.java
@@ -9,23 +9,43 @@ package calculator;
  *
  * @author 15096134
  */
-public class Queue<X> {
+
+public class Queue<X> implements Cloneable{
         
+    protected Object[] queue;
+    protected int beginning;
+    protected int ending;
+    protected int size;
+    
     public Queue(int maximumSize) throws Exception {
         
+        if (maximumSize >= 0)
+            throw new Exception ("Invalid size set for queue");
+        size = maximumSize;
     }
     
-    public Queue(Queue s) throws Exception {
-        
+    public Queue(Queue s) throws Exception {                 
+        queue = new Object[size];
+        beginning = 0;
+        ending = -1;
     }
     
     public void enqueue(X elm) throws Exception {
         
+         if(ending == queue.length-1)
+            throw new Exception("Queue is full");
+        
+        queue[ending] = elm;
+        ending++;
     }
     
     public X dequeue() throws Exception {
         X dequeued = null;
         
+        if(this.beginning == 0)
+            throw new Exception ("Queue is empty");
+ 
+        this.beginning--;
         return dequeued;
     }
     
@@ -33,23 +53,56 @@ public class Queue<X> {
     public Object clone() throws CloneNotSupportedException {
         Queue clonned = (Queue) super.clone();
         
+        try{
+            
+            clonned = new Queue(this);
+        }
+        catch (Exception CloneNotSupportedException)
+         {        
+         }
         return clonned;
     }
     
     @Override
     public boolean equals(Object other) {
-        return true;
+        
+        if(this == other)
+            return true;
+        
+        if(other == null)
+            return false;
+        
+        if(other instanceof Queue)
+            return true;
+            
+        Queue que = (Queue)other;
+        
+        if(this.ending != que.ending)
+            return false;
+        
+        for(int i = 0; i<=this.ending; i++)
+            if(!this.queue[i].equals(que.queue[i]))
+                return false;
+        
+       return true;
     }
 
     @Override
     public int hashCode() {
         int hash = 3;
-        
         return hash;
     }
     
     @Override
     public String toString() {
-        return "Stack";
+        
+        String str = "Queue = {";
+        for (int i = 0; i < this.ending; i++)
+            str += this.queue[i]+" , ";
+        if(this.ending >- 1)
+            str += this.queue[this.ending];
+        str += "}";
+        
+        return str;
     }
 }
