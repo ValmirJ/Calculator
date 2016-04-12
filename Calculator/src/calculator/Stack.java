1ad6cfefa89fcafe13230841098869e32e2f31e0
Valmir Jr <valmirmassoni@hotmail.com>
2016-04-12 08:50:54 -0300

    Pilha genérica

--- Calculator/src/calculator/Stack.java
+++ Calculator/src/calculator/Stack.java
@@ -1,55 +1,114 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator;
 
-/**
- *
- * @author 15096134
- */
+import java.lang.reflect.Method;
+
 public class Stack<X> implements Cloneable {
     
+    private int top;
+    private Object[] p;
+    
     public Stack(int maximumSize) throws Exception {
-        
+        if(maximumSize <= 0)
+            throw new Exception("Invalid Size");
+        this.p = new Object[maximumSize];
+        this.top= 0;
     }
     
-    public Stack(Stack s) throws Exception {
+    public Stack(Stack<X> another) throws Exception {
+        if(another == null)
+            throw new Exception("Null Object, copy is impossible");
         
+        this.top = another.top;
+        for(int i = 0; i < this.top; i++)
+        {
+            if(another.p[i] instanceof Cloneable)
+            {
+                Class c = p[i].getClass();
+                Class<?>[] paramsForm = null;
+                Object[] paramsReais = null;
+                Method m = c.getMethod("clone", paramsForm);
+                this.p[i] = m.invoke(another.p[i], paramsReais);
+            }
+            else 
+                this.p[i] = another.p[i];
+        }
     }
     
     public void push(X elm) throws Exception {
+        if(elm == null)
+            throw new Exception("Null Object, impossible to push");
+        if(this.top >= this.p.length)
+            throw new Exception("Stack FULL, impossible to push");
         
+        if(elm instanceof Cloneable) {
+            Class c = elm.getClass();
+            Class <?>[] paramsFormais = null;
+            Object[] paramsReais = null;
+            Method m = c.getMethod("clone", paramsFormais);
+            this.p[this.top] = (X) m.invoke(elm, paramsReais);
+        }
+        else 
+            this.p[this.top] = elm;
+        
+        this.top++;
     }
     
     public X pop() throws Exception {
-        X poped = null;
+        if(this.top <= 0)
+            throw new Exception("Empty Stack");
         
-        return poped;
+        this.top--;
+        return (X)this.p[this.top];
     }
     
     @Override
     public Object clone() throws CloneNotSupportedException {
-        Queue clonned = (Queue) super.clone();
-        
+        Stack<X> clonned = (Stack<X>) super.clone(); // ou Stack<X> clonned = null (Segundo o maligno)
+        try {
+            clonned = new Stack(this);
+        }
+        catch(Exception e) {} //Never happen, just ignore
         return clonned;
     }
     
     @Override
-    public boolean equals(Object other) {
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(!(obj instanceof Stack))
+            return false;
+        
+        Stack<X> outra = (Stack<X>) obj;
+        
+        if(this.top != outra.top)
+            return false;
+        
+        for(int i = 0; i < this.top; i++)
+            if(!(this.p[this.top].equals(outra.p[outra.top])))
+                return false;
+       
         return true;
     }
 
     @Override
     public int hashCode() {
-        int hash = 3;
+        int res = super.hashCode();
+        res  = res * 7 + new Integer(this.top).hashCode();
+        for(int i = 0; i < this.top; i++)
+            res = res * 7 + this.p[i].hashCode();
         
-        return hash;
+        return res;
     }
     
     @Override
     public String toString() {
-        return "Queue";
+        String str = "Topo: " + this.top + "\nPilha: ";
+        
+        for(int i = 0; i < this.top; i++) {
+            str += (X)this.p[i].toString();  // ***Creio*** que fazendo o cast ele vai pegar o metodo toString da classe e não da Classe Objeto
+        }
+        return str;
     }
 }
