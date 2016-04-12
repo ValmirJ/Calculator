ea1534e45340756e3f0bed8c9015ef7abb33daaa
Calebe Lustosa Carvalho <calebecarvalho@users.noreply.github.com>
2016-04-12 11:57:52 -0300

    Added files via upload

--- Calculator/src/calculator/Queue.java
+++ Calculator/src/calculator/Queue.java
@@ -9,93 +9,58 @@ package calculator;
  *
  * @author 15096134
  */
+//Guilherme, fiz as mudanças de acordo com aquilo que foi comentado
+//Tirei os seus comentários para que você possa fazer outros, caso necessário
 public class Queue<X> implements Cloneable {
 
     protected Object[] queue;
-    // Uma dica, use `first` e `last`. Assim fica claro que o primeiro é o primeiro
-    // que chegou, e ultimo, o ultimo que chegou. Se você imaginar começo como o ultimo
-    // que chegou, ja vira uma salada.
-    protected int beginning;
-    protected int ending;
-
-    // Isso não tem muita utilidade. A menos que você queira guardar a qtde
-    //  de elementos que estão na fila. Portanto:
-    // Chame isso de elementCount. Assim você não vai confundir com
-    // queue.length
-    protected int size;
+    protected int first;
+    protected int last;
+    protected int elementCount;
 
     public Queue(int maximumSize) throws Exception {
-        // Essa condição é inversa!
-        if (maximumSize >= 0) {
+       
+        if (maximumSize <= 0) {
             throw new Exception("Invalid size set for queue");
         }
-
-        // Note que ao fazer (por exemplo, no main)
-        // Queue<Integer> q = new Queue<>(20);
-        // É esse metodo que é executado (construtor) Portanto:
-        // Inicialize this.first, this.last, this.queue aqui.
-        // Não no construtor de cópia.
-        // Se for usar o elementCount, também comece-o aqui.
-        // Você não vai precisar guardar essa informação. No java, após voce inicializar
-        // um array, este ja guarda o tamanho:
-        // this.queue = new Object[maximumSize]
-        // portanto:
-        // this.queue.length sempre será igual a maximumSize
-        size = maximumSize;
+        this.queue = new Object [maximumSize];
+        this.first = 0;
+        this.last = -1;
+        this.elementCount = maximumSize;
     }
 
     public Queue(Queue s) throws Exception {
-        // Quase correto. Na verdade você terá que usar s.queue.length
-        queue = new Object[size];
-
-        // Faça o clone de todos os elementos do array.
-        // Mais precisamente, você poderia clonar apenas os elementos que estao entre
-        // this.first e this.last. Não seria simples, pois considerando a fila circular
-        // você teria que tratar o caso this.first = 5 e this.last = 1
-        //
-        // Note que se clonar o array todo, funciona e o código fica mais simples.
-        // Esses valores tem que ser a cópia de s.
-        // this.first = s.first
-        // this.last = s.last
-        // Se você usar o this.elementCount, copie-o também
-        beginning = 0;
-        ending = -1;
+       
+         queue = new Object[s.queue.length];
+         this.first = s.first;
+         this.last = s.last;
+         this.elementCount = s.elementCount;
+         this.queue = s.queue;
     }
 
     public void enqueue(X elm) throws Exception {
-        // Se fizer uma fila cirluar, e você usar o elementCount
-        // você poderá fazer:
-        // if (elementCount >= queue.length)
-        //     throw new Exception("Queue is full");
-
-        // Tente fazer a fila circular.
-        if (ending == queue.length - 1) {
+       
+        if (elementCount >= queue.length) {
             throw new Exception("Queue is full");
         }
-
-        // Note que você começou ending com -1. Isso vai dar erro.
-        // Incremente antes, ou começe ending (last) com 0
-        queue[ending] = elm;
-        ending++;
-
-        // Também incremente o elementCount se você usá-lo
+        if(last == queue.length - 1)
+            last = -1;
+        this.last++;
+        this.elementCount++;
+        this.queue[last] = elm;
     }
 
     public X dequeue() throws Exception {
-        X dequeued = null;
-
-        // Isso sempre vai dar erro.
-        // Você iniciou o beginning com 0, e esta atualizando somente ending
-        // no enqueue. Portanto o beginning sempre será 0.
-        // Note que se você usar o elementCount, você pode fazer
-        // if (this.elementCount <= 0)
-        if (this.beginning == 0) {
+        X dequeued; 
+          dequeued = (X) this.queue[this.first];
+          
+        if (this.elementCount <= 0) 
             throw new Exception("Queue is empty");
-        }
-
-        this.beginning--;
-
-        // Faltou retornar o elemento correto. Esse mock devolverá null
+        if (this.first == queue.length)
+            first = 0;
+        
+        this.first++;
+        this.elementCount--;
         return dequeued;
     }
 
@@ -129,21 +94,19 @@ public class Queue<X> implements Cloneable {
         }
 
         Queue que = (Queue) other;
-
-        if (this.ending != que.ending) {
+        
+        if (this.first != que.first){
             return false;
         }
-
-        // faltou checar o first (beginning)
-        // Hmm, isso não vai rolar. Considere que numa file circular
-        // podem sobrar elementos no array pois estarão fora do beginning e ending
-        // Eles não devem ser considerados nessa comparação
-        for (int i = 0; i <= this.ending; i++) {
+        
+        if (this.last != que.last) {
+            return false;
+        }
+        for (int i = this.first; i <= this.last; i++) {
             if (!this.queue[i].equals(que.queue[i])) {
                 return false;
             }
         }
-
         return true;
     }
 
@@ -157,15 +120,13 @@ public class Queue<X> implements Cloneable {
     public String toString() {
 
         String str = "Queue = {";
-        // Você está mostrando o array da fila, não o first até o last
-        for (int i = 0; i < this.ending; i++) {
+        for (int i = this.first; i < this.last; i++) {
             str += this.queue[i] + " , ";
         }
-        if (this.ending > - 1) {
-            str += this.queue[this.ending];
+        if (this.last > - 1) {
+            str += this.queue[this.last];
         }
         str += "}";
-
         return str;
     }
 }
